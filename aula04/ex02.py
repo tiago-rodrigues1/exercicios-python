0d7f04ab1f346215de45a6af8e42681cd0ff09d5
tiago-rodrigues1 <tiagords127@gmail.com>
2022-02-22 16:21:58 -0300

    add primeiros exercicios

---
+++ aula04/ex02.py
@@ -0,0 +1,5 @@
+dia = input('- Dia: ')
+mes = input('- Mes: ')
+ano = input('- Ano: ')
+
+print('Você nasceu dia', dia, 'de', mes,'de', ano, '. Correto ?')
---
+++ aula04/ex03.py
@@ -0,0 +1,6 @@
+num1 = int ( input('- Digite um número: ') )
+num2 = int ( input('- Digite outro número: ') )
+
+soma = num1 + num2
+
+print(num1 , '+' , num2 , '=' , soma)
