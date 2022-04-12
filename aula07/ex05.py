e3c94296efd806ab1cb5870636090bec45f857af
Tiago Rodrigues <tiagods127@gmail.com>
2022-04-12 11:51:18 -0300

    fead: add exercicio 05

---
+++ aula07/ex05.py
@@ -0,0 +1,7 @@
+num = int(input('- Digite um número: '))
+
+sucessor = num + 1
+antecessor = num - 1
+
+print('\nAntecessor de {} = {}'.format(num, antecessor))
+print('Sucessor de {} = {}'.format(num, sucessor))
