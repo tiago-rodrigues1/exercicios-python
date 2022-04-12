75c6eedb81aa5b7006ca6d891c10f174be5c71c6
Tiago Rodrigues <tiagods127@gmail.com>
2022-04-12 11:51:22 -0300

    fead: add exercicio 06

---
+++ aula07/ex06.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+
+num = int(input('- Digite um número: '))
+
+dobro = num * 2
+triplo = num * 3
+raiz = sqrt(num)
+
+print('\n{} * 2 = {}'.format(num, dobro))
+print('{} * 3 = {}'.format(num, triplo))
+print('√{} = {}'.format(num, raiz))
