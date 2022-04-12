c088a3dd1998f0955ed5e1ead2878cd3aac2dd7d
Tiago Rodrigues <tiagods127@gmail.com>
2022-04-12 11:51:48 -0300

    fead: add exercicio 10

---
+++ aula07/ex10.py
@@ -0,0 +1,6 @@
+quantidadeReais = float(input('- Quantos reais você tem na carteira: '))
+
+valor1Dolar = 3.27
+dolares = quantidadeReais / valor1Dolar
+
+print('\nVocê pode comprar {:.2f} dólar(es)'.format(dolares))
