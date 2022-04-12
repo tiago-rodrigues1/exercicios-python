faf7d77f4aa22650d5680c9c5084bdf44c60605a
Tiago Rodrigues <tiagods127@gmail.com>
2022-04-12 11:51:33 -0300

    fead: add exercicio 08

---
+++ aula07/ex08.py
@@ -0,0 +1,7 @@
+metros = float(input('- Digite o valor em metros: '))
+
+centimetros = metros * 100
+milimetros = metros * 1000
+
+print('\n{} m = {} cm'.format(metros, centimetros))
+print('{} m = {} mm'.format(metros, milimetros))
