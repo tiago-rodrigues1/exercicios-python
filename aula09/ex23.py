667c5ca5d7645b0b01b56eb2c115ae629c807d4b
tiago-rodrigues1 <tiagords127@gmail.com>
2022-04-23 20:18:43 -0300

    feat: add exercicio 23

---
+++ aula09/ex23.py
@@ -0,0 +1,13 @@
+numero = int(input('- Informe um número: '))
+
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+
+print('Unidades: {}'.format(u))
+print('Dezenas: {}'.format(d))
+print('Centenas: {}'.format(c))
+print('Milhar: {}'.format(m))
+
+
